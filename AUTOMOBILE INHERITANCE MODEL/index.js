1751e90873d420d5f40ef0b5acf6d379e87b5993
pravin7878 <143276463+pravin7878@users.noreply.github.com>
2023-12-03 14:00:54 +0530

    code pushing

---
+++ AUTOMOBILE INHERITANCE MODEL/index.js
@@ -0,0 +1,21 @@
+let Fourwheelsvehical = {
+    speed: 240,
+    fiultype: 'petrol',
+    numofwiheels: 4
+}
+
+   function Car(model,brands,typeofengin){
+    this.model = model;
+    this.brands = brands;
+    this.typeofengin = typeofengin;
+}
+
+Car.prototype = Fourwheelsvehical
+let car1 =new Car(2023,'tata', 'non electric')
+console.log(car1)
+
+let car2 =new Car(2024,'bmw', 'non electric')
+console.log(car2)
+
+car2.speed = 350;
+
